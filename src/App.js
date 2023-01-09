77022d342189d845f78d3167026e16692ee283bb
zsolt115 <sikozsolt96@gmail.com>
2023-01-09 14:29:48 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cakes: [
+        {
+          name: 'cheesecake',
+          id: '1'
+        },
+        {
+          name: 'tiramisu',
+          id: '2'
+        },
+        {
+          name: 'chocolate cake',
+          id: '3'
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className='App'>
+        {
+          this.state.cakes.map(cake => <h1>{cake.name}</h1>)
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
